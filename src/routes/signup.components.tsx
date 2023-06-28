74ec1461025f52f69887aba89d6d8df494449e6f
Thibetanus <steventhibe@gmail.com>
2023-06-29 01:04:03 +0800

    feat(signup): add signup route

---
+++ src/routes/signup.components.tsx
@@ -0,0 +1,14 @@
+import SignUpForm from '../components/sign-up-form/sign-up-form.component';
+
+const SignUp = () => {
+  return (
+    <>
+      <main className="flex flex-col items-center">
+        <h1>註冊頁</h1>
+        <SignUpForm />
+      </main>
+    </>
+  );
+};
+
+export default SignUp;
